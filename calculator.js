c9094a6f3aff4e4f39bccc257e7391035d13b77e
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-18 10:14:24 -0800

    Set up add, subtract, multiply, divide functions

---
+++ calculator.js
@@ -0,0 +1,21 @@
+// basic functions add, subtract, multiply, divide
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+console.log(add(3, 4));
+console.log(subtract(5, 2));
+console.log(multiply(4, 5));
+console.log(divide(6, 4));
