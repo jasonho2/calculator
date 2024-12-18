0b3a64818f15a29c9a10509881cfa8d257fab156
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-18 12:38:30 -0800

    Update to 2x2 flex grid and add click event listeners

--- calculator.js
+++ calculator.js
@@ -44,7 +44,7 @@ function operate(firstInput, operator, secondInput) {
     }
 };
 
-console.log(operate(3, '+', 5));
+// console.log(operate(3, '+', 5));
 
 // create the body container
 const calculatorContainer = document.createElement("div");
@@ -54,16 +54,26 @@ document.body.appendChild(calculatorContainer);
 // create a display for the entered digits and operators - like a calculator screen
 const calculatorDisplay = document.createElement("div");
 calculatorDisplay.id = "calculator-display";
-calculatorDisplay.textContent = "2 + 2 = 4"; // placeholder text
+calculatorDisplay.classList.add("flexbox-item");
+calculatorDisplay.textContent = "Start calculating!"; // placeholder text
 calculatorContainer.appendChild(calculatorDisplay);
+
+// create a clear button
+const clearButton = document.createElement("button");
+clearButton.id = "clear-button";
+clearButton.classList.add("flexbox-item");
+clearButton.textContent = "Clear";
+calculatorContainer.appendChild(clearButton);
 
 // create separate containers for the digit buttons and operator buttons
 const digitContainer = document.createElement("div");
 digitContainer.id = "digit-container";
+digitContainer.classList.add("flexbox-item");
 calculatorContainer.appendChild(digitContainer);
 
 const operatorContainer = document.createElement("div");
 operatorContainer.id = "operator-container";
+operatorContainer.classList.add("flexbox-item");
 calculatorContainer.appendChild(operatorContainer);
 
 // create the calculator buttons
@@ -76,7 +86,7 @@ for (let i = 0; i < calculatorDigits.length; i++) {
     button.classList.add("calculator-digit-button");
     button.textContent = calculatorDigits[i];
     digitContainer.appendChild(button);
-}
+};
 
 // append the operator buttons
 for (let i = 0; i < calculatorOperators.length; i++) {
@@ -84,4 +94,20 @@ for (let i = 0; i < calculatorOperators.length; i++) {
     button.classList.add("calculator-operator-button");
     button.textContent = calculatorOperators[i];
     operatorContainer.appendChild(button);
-}
+};
+
+// event listener that shows the digit of the button being pressed
+digitContainer.addEventListener('click', (event) => {
+    // check if the element clicked is a button
+    if (event.target.tagName === 'BUTTON'){
+        console.log(event.target.textContent); // show the text of the button clicked
+    };
+});
+
+// event listener that shows the operator of the button being pressed
+operatorContainer.addEventListener('click', (event) => {
+    // check if the element clicked is a button
+    if (event.target.tagName === 'BUTTON'){
+        console.log(event.target.textContent); // show the text of the button clicked
+    };
+});
