be07a0ac9bbb1bbf718e8808902f604f1bc97db1
jasonho2 <31460284+jasonho2@users.noreply.github.com>
2024-12-18 10:33:32 -0800

    Set up operate function

--- calculator.js
+++ calculator.js
@@ -15,7 +15,33 @@ function divide(a, b) {
     return a / b;
 };
 
-console.log(add(3, 4));
-console.log(subtract(5, 2));
-console.log(multiply(4, 5));
-console.log(divide(6, 4));
+// console.log(add(3, 4));
+// console.log(subtract(5, 2));
+// console.log(multiply(4, 5));
+// console.log(divide(6, 4));
+
+// let firstInput = 0;
+// let operator = '';
+// let secondInput = 0;
+
+// function that calls the above 4 functions depending on the operator
+function operate(firstInput, operator, secondInput) {
+
+    eligibleOperators = ['+','-','*','/'];
+
+    if (!eligibleOperators.includes(operator)) {
+        alert('Please use an eligible operator: +, -, *, /');
+    }
+
+    if (operator === '+') {
+        return add(firstInput, secondInput);
+    } else if (operator === '-') {
+        return subtract(firstInput, secondInput);
+    } else if (operator === '*') {
+        return multiply(firstInput, secondInput);
+    } else { // divide
+        return divide(firstInput, secondInput);
+    }
+};
+
+console.log(operate(3, '+', 5));
